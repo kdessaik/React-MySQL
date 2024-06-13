5482bb635ff133cd38010a4e19eb95b495a9191b
Kambale Dessai <93641789+kdessaik@users.noreply.github.com>
2024-06-14 00:06:42 +0300

    changes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,33 +1,68 @@
 import React, { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  [data,setData]=useState()
+ 
+ [data,setData]=useState() 
   useEffect(()=>{
-    fetch('http://localhost:8081/usertable')
-    .then(res=>res.json())
-    .then(data => setData(data))
-    .catch(err => console.log(err))
-  },[])
-  return (
-    <div style={{color:'white'}}>
-    <table>
-      <thead>
-        <th>
-          Id
-        </th>
-        <th>
-          Name
-        </th>
+        fetch('http://localhost:8081/usertable')
+        .then(res=>res.json())
+        .then(data => setData(data))
+        .catch(err => console.log(err))
+      },[])
       
-      <th>
-          Phone
-        </th>
-        <th>
-          Email
-        </th>
-        </thead>
-    </table>
-    </div>
+    let de=data
+    console.log(de[0].id)
+  
+
+  return (
+
+
+    
+    
+
+
+
+    <>
+      
+   
+      
+  
+        <div>
+        <table>
+          <thead>
+            <th>
+              Id
+            </th>
+            <th>
+              Name
+            </th>
+          
+          <th>
+              Phone
+            </th>
+            <th>
+              Email
+            </th>
+            </thead>
+            <tbody>
+              <tr><td></td></tr>
+              {data.map((d,i)=>{
+                <tr key={i}>
+                  <td>{d.id}</td>
+                </tr>
+              }
+              )}
+              
+            </tbody>
+        </table>
+        </div>
+      
+
+     
+    </>
   )
 }
 
